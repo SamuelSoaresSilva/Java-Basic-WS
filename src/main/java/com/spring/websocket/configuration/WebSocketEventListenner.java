7fecc383cda6ab7e810fa23cf603774de82fd431
samuelsoaressilva <samuelsoaressilva.dev@gmail.com>
2025-09-09 23:51:24 -0300

    Impede a alocação de valor nulo na variáve username em handlerWebSocketDisconnectListenner

--- src/main/java/com/spring/websocket/configuration/WebSocketEventListenner.java
+++ src/main/java/com/spring/websocket/configuration/WebSocketEventListenner.java
@@ -9,6 +9,8 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.Objects;
+
 @Component
 public class WebSocketEventListenner {
 
@@ -26,7 +28,7 @@ public class WebSocketEventListenner {
             ){
 
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
         if (username != null){
             log.info("User disconnected {}", username);
             ChatMessage message = new ChatMessage(MessageType.LEAVE, username);
